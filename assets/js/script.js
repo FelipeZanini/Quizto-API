bad1e3ef018fb6d937d1dcd5d51592b4742a52ed
Felipe Silva <felipe_zanini@hotmail.com>
2023-05-14 23:14:18 +0000

    add DOMContentLoaded to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-console.log('meu paui');
+document.addEventListener("DOMContentLoaded", function() {
+    for(i = 0; i < 5; i++){
+        let counter = document.getElementById('question-counter');
+        counter.innerHTML = ++i;
+    }
+        });
