5b6d10b4a6801ec928026d5a9ee8764bc2554a92
Felipe Silva <felipe_zanini@hotmail.com>
2023-05-15 12:39:00 +0000

    get elements from HTML to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    for(i = 0; i < 5; i++){
-        let counter = document.getElementById('question-counter');
-        counter.innerHTML = ++i;
+//get all elements on html document
+let questionCounter = document.getElementById('question-counter');
+let question = document.getElementById('question');
+let answer1 = document.getElementById('answer-1');
+let answer2 = document.getElementById('answer-2');
+let answer3 = document.getElementById('answer-3');
+let answer4 = document.getElementById('answer-4');
+let nextQuestion = document.getElementById('next-question');
+//start of quiz
+const counter = 0;
+function newQuestion(counter){
+    if (counter == 0){
+        questionCounter.innerHTML = ++counter;
+        question.textContent = "What is H20?";
+        answer1.textContent = "Water";
+        answer2.textContent = "Aluminium";
+        answer3.textContent = "Iron";
+        answer4.textContent = "Oxygen";
+    }//elseif(counter==1){do the other question} 
+}
+newQuestion(counter); //call the question start 
+answer1.addEventListener('click', function(){
+    if(answer1.textContent == 'Water') {
+        console.log('you did it right');
     }
-        });
+} )
+
