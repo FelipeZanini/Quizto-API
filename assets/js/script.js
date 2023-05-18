2e2dc67e095d287dc0192c966bc32213694a1262
Felipe Silva <felipe_zanini@hotmail.com>
2023-05-18 19:56:53 +0000

    add class list right and wrong, then removing for the next question

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ let answers = document.getElementsByClassName('answers');
 let nextQuestion = document.getElementById('next-question');
 
 //global variables
-let score, counter;
+let score, counter; 
 
 //Question for our quizz
 const questions = [
@@ -22,7 +22,7 @@ const questions = [
     {
         question: 'What is jujubinha',
         answers: [
-        
+
             { text: 'A Bird', correct: false },
             { text: 'A Dog', correct: true },
             { text: 'A Plane', correct: false },
@@ -36,7 +36,7 @@ startQuiz();
 //Start quiz function
 function startQuiz() {
     score = 0;
-    counter = 1;
+    counter = 0;
     displayQuestion(counter);
     //gameOver () // if we arrive at the end, each function should be linked!
 }
@@ -44,24 +44,36 @@ function startQuiz() {
 //Display Question function
 function displayQuestion(counter) {
 
-    questionCounter.innerText = counter;
+    questionCounter.innerText = `${1 + counter}/5`;
     question.innerText = questions[counter].question;
 
     for (let i = 0; i < answers.length; i++) {
         answers[i].innerText = questions[counter].answers[i].text;
-        if(questions[counter].answers[i].correct){
+        if (questions[counter].answers[i].correct) {
             answers[i].setAttribute("datatype", "right");
-        }    
+        }
     }
 }
 
 //Display Check answer function
 function checkAnswer(answer) {
-console.log(answer); 
-if(answer.getAttribute("datatype", "right")){
-        alert('that is the right one');
-    
-}
-   
+    if (answer.getAttribute("datatype", "right")) {
+        answer.classList.add("right");
+        counter++;
+        score++;
+        nextQuestion.classList.remove('hide');
+    } else {
+        answer.classList.add("wrong");
+        //answer.getAttribute("datatype", "right".click());
+    }
+
 }
 
+function nextButtom() {
+    for (let i = 0; i < answers.length; i++) {
+        answers[i].classList.remove("wrong", "right");
+        answers[i].removeAttribute("datatype", "right");
+    }
+    nextQuestion.classList.add('hide');
+    displayQuestion(counter);
+}
