5b2ca25fef3821e433af9ec7737676da7423d90d
Felipe Silva <felipe_zanini@hotmail.com>
2023-05-23 20:53:47 +0000

    add comments to the function on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,16 +71,27 @@ let score, counter;
 
 // Wait for the DOM to finish loading to display the land page
 document.addEventListener("DOMContentLoaded", function () {
-
-    startButton.classList.remove('hide');
-    gameOverText.classList.remove('hide');
-    LogoLandPage.classList.remove('hide');
+    
     gameOverText.innerHTML = `<strong>Test Your Knowledge! <i class="far fa-lightbulb"></i></strong> `;
 })
 
 /**
- * The main game "loop", called when the script is first loaded
- * and after the user's answer has been processed
+ * Is called when the user fires the start or restart button,
+ * hiding the land page or game over page to the user,
+ * calling the 'startquiz' to properly start the quiz */
+function setUpQuiz() {
+    for (let i = 0; i < 2; i++) {
+        quizBox.children[i].classList.remove('hide');
+    }
+    gameOverText.classList.add('hide');
+    startButton.classList.add('hide')
+    LogoLandPage.classList.add('hide');;
+    startQuiz()
+}
+
+/**
+ * The quiz start function,
+ * is called when the user wants to start or restart the quiz
  */
 function startQuiz() {
     score = 0;
@@ -88,12 +99,14 @@ function startQuiz() {
     displayQuestion(counter);
 }
 
-//Display Question function
+/**
+ * Display question and answers to the user,
+ * also set the right answers to the respectively question
+ */
 function displayQuestion(counter) {
 
     questionCounter.innerHTML = `${1 + counter}`;
     question.innerText = questions[counter].question;
-    //OTHER FUNCTION TO ADD ATRIBUTE TO THE RIGHT ANSWER, CALLED SetRightAnswer
     for (let i = 0; i < answers.length; i++) {
         answers[i].innerText = questions[counter].answers[i].text;
         if (questions[counter].answers[i].correct) {
@@ -102,37 +115,61 @@ function displayQuestion(counter) {
     }
 }
 
-//Display Check answer function
+/**
+ * Get the clicked answer from the user,
+ * giving a visual feedback if the user clicked on the right or wrong one,
+ * stoping any intereraction with the answers after that 
+ */
 function checkAnswer(answer) {
+    
     if (answer.getAttribute("datatype", "right")) {
         answer.classList.add("right");
         score++;
     } else {
         answer.classList.add("wrong");
     }
-    //remove pointer events function stopUserInteraction
+    // Called to stop any user interaction with the answers, and show the right answers
+    stopUserInteraction();
+    
+    counter++;
+    // Show the next question button to the user
+    nextQuestion.classList.remove('hide');
+}
+/**
+ * This function is made to show the user the right answer if he/she failed, 
+ * then stoping any pointer events (interactions) from the user
+ */
+function stopUserInteraction(){
     for (let i = 0; i < answers.length; i++) {
         answers[i].style.pointerEvents = 'none';
         if (answers[i].getAttribute("datatype", "right")) {
             answers[i].classList.add("right");
         }
     }
-    nextQuestion.classList.remove('hide');
-    counter++;
 }
-
+/**
+ * This function is called when the user fires the next button, 
+ * setting the answers to default, removing class and data type,
+ * calling the gameOver function to check if the quiz is finished, if not it calls the displayQuestion
+ */
 function nextPage() {
+    // Hide the next question button
+    nextQuestion.classList.add('hide');
+    
     for (let i = 0; i < answers.length; i++) {
         answers[i].classList.remove("wrong", "right");
         answers[i].removeAttribute("datatype", "right");
         answers[i].style.pointerEvents = 'auto';
     }
-    nextQuestion.classList.add('hide');
     gameOver();
     displayQuestion(counter);
 }
 
-//game over function
+/**
+ * Is called every time the user wants to proceed to next question,
+ * if the game is over, is displayed the game over page,
+ * showing the score and the restart button to the user
+ */
 function gameOver() {
     if (counter == questions.length) {
         for (let i = 0; i < 2; i++) {
@@ -146,12 +183,3 @@ function gameOver() {
     }
 }
 
-function restartQuiz() {
-    for (let i = 0; i < 2; i++) {
-        quizBox.children[i].classList.remove('hide');
-    }
-    gameOverText.classList.add('hide');
-    startButton.classList.add('hide')
-    LogoLandPage.classList.add('hide');;
-    startQuiz()
-}
